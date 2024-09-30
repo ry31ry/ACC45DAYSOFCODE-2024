3f9be1468260eec82efb371e50187688e396c476
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-09-30 12:09:22 +0530

    Add files via upload

---
+++ Day 3-OFFICE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    int t;
+    cin>>t;
+    while(t--){
+    cin>>a;
+    cin>>b;
+    
+    cout<<(a*4)+b<<endl;
+    }
+    return 0;
+}
