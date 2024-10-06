97ff77c10b75a0d806219e2e856b95afd6bc7eee
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-10-06 22:38:36 +0530

    Add files via upload

---
+++ Day 9-CHRISTGREET.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	if(x==25){
+	    cout<<"Christmas"<<endl;
+	}else{
+	    cout<<"Ordinary"<<endl;
+	}
+return 0;
+}
