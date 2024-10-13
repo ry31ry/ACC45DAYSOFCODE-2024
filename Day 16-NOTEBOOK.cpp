92d33a163d0bf640e4ebb932d157858f11ee09d7
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-10-13 16:55:11 +0530

    Add files via upload

---
+++ Day 16-NOTEBOOK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	int n;
+	//testcase
+	cin>>t;
+	while(t--){
+	    cin>>n; //weight of pulp
+	    int notebook=n*10;
+	    // num of notebooks
+	    cout<<notebook<<endl;
+	}
+return 0;
+}
