33ee7a0068b87690d38d84472a5025c63a4b1db3
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-10-29 14:26:14 +0530

    Add files via upload

---
+++ Day 27-CHEFSTEP.py
@@ -0,0 +1,12 @@
+# cook your dish here
+T = int(input())
+while T:
+    T -= 1
+    N, K = map(int, input().split())
+    D = list(map(int, input().split()))
+    for i in D:
+        if int(i) % K == 0:
+            print(1, end='')
+        else:
+            print(0, end='')
+    print()
