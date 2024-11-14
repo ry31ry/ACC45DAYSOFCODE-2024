80266e5371ad3b84a58d3707a1560b5b1d86e854
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-11-14 20:58:44 +0530

    Add files via upload

---
+++ Day 43-PASSORFAIL.py
@@ -0,0 +1,11 @@
+# cook your dish here
+T=int(input()) 
+for i in range(T) :
+    N, X, P=map(int, input(). split()) 
+    C=(3*X) 
+    D=(N-X) *1 
+    R=C-D
+    if(R>=P) :
+        print("PASS") 
+    else:
+        print("FAIL")
