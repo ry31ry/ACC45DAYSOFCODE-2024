c92b38d1225d3c77a96494dc0b484bb650560090
ry31ry <145043134+ry31ry@users.noreply.github.com>
2024-11-16 18:41:28 +0530

    Add files via upload

---
+++ Day 45-PRESENTS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int c=n/5;
+	    cout<<(n-c)<<"\n";
+	}
+	return 0;
+}
